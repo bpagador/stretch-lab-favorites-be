a03ff59486f33e9d417b7a573c3482ab5496ab2f
Briseid Pagador <bpagador@gmail.com>
2020-05-13 14:23:24 -0700

    setup complete

--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -1,6 +1,6 @@
 const client = require('../lib/client');
 // import our seed data:
-const animals = require('./animals.js');
+const favorites = require('./favorites.js');
 const usersData = require('./users.js');
 const { getEmoji } = require('../lib/emoji.js');
 
@@ -25,12 +25,12 @@ async function run() {
     const user = users[0].rows[0];
 
     await Promise.all(
-      animals.map(animal => {
+      favorites.map(favorite => {
         return client.query(`
-                    INSERT INTO animals (name, cool_factor, owner_id)
-                    VALUES ($1, $2, $3);
+                    INSERT INTO favorites (name, full_name, alter_egos, place_of_birth, first_appearance, publisher, alignment, owner_id)
+                    VALUES ($1, $2, $3, $4, $5, $6, $7, $8);
                 `,
-        [animal.name, animal.cool_factor, user.id]);
+        [favorite.name, favorite.full_name, favorite.alter_egos, favorite.place_of_birth, favorite.first_appearance, favorite.publisher, favorite.alignment, user.id]);
       })
     );
     
